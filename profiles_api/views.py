b5d266ba847c465a4de10a693c5aad6dd490bcae
rupesh.sharma@stryker.com <rupesh.sharma@stryker.com>
2021-05-11 08:07:27 +0530

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -83,10 +86,18 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle updating an object"""
         return Response({'http_method': 'PUT'})
 
-    def partial_updatw(self, request, pk=None):
+    def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
         return Response({'http_method': 'PATCH'})
 
     def destroy(self, request, pk=None):
         """Handle Removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
